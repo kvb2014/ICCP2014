4e648ddab1ed0040e6e2a8ea01557fe7dcd07db2
vandent6 <vandent6@msu.edu>
2014-05-07 10:24:02 -0400

    Jos helped with it. Code is now running fine.

--- twolevel.f95
+++ twolevel.f95
@@ -7,7 +7,8 @@ program twolevel
    !-- Simulation Variables
    complex*16 :: g_omega0
    complex*16 :: g_freq
-   complex*16 :: g_period
+   complex*16 :: g_period1
+   complex*16 :: g_period2
    
    !-- Quantum Variables
    complex*16 :: g_sigmax(2,2), g_sigmaz(2,2), g_sigmay(2,2) !Pauli matrices
@@ -27,25 +28,32 @@ program twolevel
    call setUpVariables()
    do i = 0,g_duration
       call calcPsi()
-      g_t = g_t + g_dt
    end do
    
 contains
    
    subroutine calcPsi
       complex*16 :: up(2) = (/dcmplx(0d0,0d0), dcmplx(1d0,0d0)/)
-      
-      g_Ham = g_omega0/2d0 * g_sigmaz + g_period * sin(g_freq * g_t) * g_sigmax
-      
-      g_HamNumer = dcmplx(0d0,2d0)*g_unity + g_Ham
-      g_HamDenom = dcmplx(0d0,2d0)*g_unity - g_Ham
+      complex*16 :: down(2) = (/dcmplx(1d0,0d0), dcmplx(0d0,0d0)/), ii=cmplx(0._8,1._8)
+
+      g_Ham = g_omega0/2d0 * g_sigmaz + g_period1 * sin(g_freq * g_t) * g_sigmay &
+         + g_period2 * cos(g_freq * g_t) * g_sigmax   
+   
+      g_HamNumer = 1*g_unity - g_Ham * g_dt *II*0.5_8
+
+      g_t = g_t + g_dt
+
+      g_Ham = g_omega0/2d0 * g_sigmaz + g_period1 * sin(g_freq * g_t) * g_sigmay &
+         + g_period2 * cos(g_freq * g_t) * g_sigmax
+
+      g_HamDenom = 1*g_unity + g_Ham * g_dt * II*0.5_8
       call invertComplex(g_HamDenom)
       
       g_Ham = matmul(g_HamDenom,g_HamNumer)
       
       g_psi = matmul(g_Ham,g_psi)
       
-      print *, abs(dot_product(up,g_psi))
+      print *, dble(g_psi(1)*conjg(g_psi(1)))
       
    end subroutine calcPsi
    
@@ -68,15 +76,18 @@ contains
       !- Define the simulation constants
       g_omega0       = dcmplx(1d0,0d0);
       g_freq         = dcmplx(1d0,0d0);
-      g_period       = dcmplx(1d0,0d0);
+      g_period1      = dcmplx(1d0,0d0);
+      g_period2      = dcmplx(1d0,0d0);
       
       !- Define the time-related variables
       g_dt  = 0.01d0
       g_t   = 0d0
-      g_duration = 1000      
+      g_duration = 1000
       
       !- Set up the inital wavefunction (the up state)
-      g_psi = (/ dcmplx(0d0,0d0), dcmplx(1d0,0d0) /)
+      g_psi = (/ dcmplx(1d0,0d0), dcmplx(0d0,0d0) /)
+      !- Set up the inital wavefunction (the down state)
+      !g_psi = (/ dcmplx(1d0,0d0), dcmplx(0d0,0d0) /)
       
    end subroutine setUpVariables
    
